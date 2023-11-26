36727b5b0b2f397166c2552725c4c5b156467f7e
Izendi <cjwinters1404@gmail.com>
2023-11-26 16:19:54 +0100

    added camera class from learnOpenGl website.

--- zenditeEngineV2/src/main.cpp
+++ zenditeEngineV2/src/main.cpp
@@ -7,10 +7,26 @@
 #include "Texture2D.h"
 #include "geometrySetup.h"
 #include "menu.h"
+#include "Camera.h"
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void processInput(GLFWwindow* window);
 
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
+
+// camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+bool firstMouse = true;
+
+// timing
+float deltaTime = 0.0f;	// time between current frame and last frame
+float lastFrame = 0.0f;
 
 struct objPos
 {
@@ -33,7 +49,15 @@ int main(void)
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+
+	// tell GLFW to capture our mouse
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
 	glfwSwapInterval(1);
+
 	if (glewInit() != GLEW_OK)
 	{
 		std::cout << "I'm am not GLEW_OK, I'm GLEW_SAD :(\n";
@@ -43,6 +67,20 @@ int main(void)
 	bool toggle = true;
 
 	std::cout <<glGetString(GL_VERSION) << "\n";
+
+	glm::vec3 cubePositions[] = 
+	{
+		glm::vec3(0.0f,  0.0f,  0.0f),
+		glm::vec3(2.0f,  5.0f, -15.0f),
+		glm::vec3(-1.5f, -2.2f, -2.5f),
+		glm::vec3(-3.8f, -2.0f, -12.3f),
+		glm::vec3(2.4f, -0.4f, -3.5f),
+		glm::vec3(-1.7f,  3.0f, -7.5f),
+		glm::vec3(1.3f, -2.0f, -2.5f),
+		glm::vec3(1.5f,  2.0f, -2.5f),
+		glm::vec3(1.5f,  0.2f, -1.5f),
+		glm::vec3(-1.3f,  1.0f, -1.5f)
+	};
 
 	//Setup Shaders:
 	Shader shader_1("C:/Code/Chalmers/myGraphicsCode/zenditeEngineV2/zenditeEngineV2/res/shaders/vs_Basic.glsl", 
@@ -100,15 +138,18 @@ int main(void)
 
 	while (!glfwWindowShouldClose(window))
 	{
+		processInput(window);
+
 		count++;
 		/* Render here */
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		shader_Transform.bindProgram();
-		float timeValue = glfwGetTime();
-		//float greenValue = sin(timeValue) / 2.0f + 0.5f;
-		//shader_1.setUniform4f("ourColor", 0.1f, greenValue, 0.1f, 1.0f);
+
+		float currentFrame = static_cast<float>(glfwGetTime());
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
 
 		//Set up Transform Matrices each frame -- START --
 		square.pos = glm::vec3(0.0f, 0.0f, 0.0f);
@@ -118,29 +159,38 @@ int main(void)
 		square.scale = 1.0f;
 
 		glm::mat4 modelMat = glm::mat4(1.0f);
-		modelMat = glm::translate(modelMat, square.pos);
-		modelMat = glm::rotate(modelMat, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+		//modelMat = glm::translate(modelMat, square.pos);
+		//modelMat = glm::rotate(modelMat, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
 		//modelMat = glm::rotate(modelMat, glm::radians(square.xRot), glm::vec3(1.0f, 0.0f, 0.0f));
 		//modelMat = glm::rotate(modelMat, glm::radians(square.yRot), glm::vec3(0.0f, 1.0f, 0.0f));
 		//modelMat = glm::rotate(modelMat, glm::radians(square.zRot), glm::vec3(0.0f, 0.0f, 1.0f));
-		modelMat = glm::scale(modelMat, glm::vec3(square.scale, square.scale, square.scale));
+		//modelMat = glm::scale(modelMat, glm::vec3(square.scale, square.scale, square.scale));
 
-		glm::mat4 viewMat = glm::mat4(1.0f);
-		viewMat = glm::translate(viewMat, glm::vec3(0.0f, 0.0f, -3.0f));
+		glm::mat4 viewMat = camera.GetViewMatrix();
 
-		glm::mat4 projMat = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+		glm::mat4 projMat = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 
-		shader_Transform.setUniformMat4("modelMat", GL_FALSE, glm::value_ptr(modelMat));
+		//shader_Transform.setUniformMat4("modelMat", GL_FALSE, glm::value_ptr(modelMat));
 		shader_Transform.setUniformMat4("viewMat", GL_FALSE, glm::value_ptr(viewMat));
 		shader_Transform.setUniformMat4("projMat", GL_FALSE, glm::value_ptr(projMat));
 
 		// -- END --
-
 		
 		//Draw Call HERE
 		bindVao(VAO_Cube);
 
-		GLCALL(glDrawArrays(GL_TRIANGLES, 0, 36));
+		for (unsigned int i = 0; i < 10; i++)
+		{
+			modelMat = glm::mat4(1.0f);
+			modelMat = glm::translate(modelMat, cubePositions[i]);
+			float angle = 20.0f * i;
+			modelMat = glm::rotate(modelMat, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+			shader_Transform.setUniformMat4("modelMat", GL_FALSE, glm::value_ptr(modelMat));
+
+			GLCALL(glDrawArrays(GL_TRIANGLES, 0, 36));
+
+		}
+
 		
 		//GLCALL(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0));
 
@@ -185,4 +235,72 @@ int main(void)
 
 	glfwTerminate();
 	return 0;
+}
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.ProcessKeyboard(FORWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.ProcessKeyboard(BACKWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.ProcessKeyboard(LEFT, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.ProcessKeyboard(RIGHT, deltaTime);
+
+	if(glfwGetKey(window, GLFW_KEY_C) == GLFW_PRESS)
+	{
+		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+		glfwSetCursorPosCallback(window, nullptr);
+	}
+	if (glfwGetKey(window, GLFW_KEY_V) == GLFW_PRESS)
+	{
+		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+		glfwSetCursorPosCallback(window, mouse_callback);
+	}
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// make sure the viewport matches the new window dimensions; note that width and 
+	// height will be significantly larger than specified on retina displays.
+	glViewport(0, 0, width, height);
+}
+
+
+// glfw: whenever the mouse moves, this callback is called
+// -------------------------------------------------------
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
+{
+	
+	float xpos = static_cast<float>(xposIn);
+	float ypos = static_cast<float>(yposIn);
+
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+	lastX = xpos;
+	lastY = ypos;
+
+	camera.ProcessMouseMovement(xoffset, yoffset);
+	
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	camera.ProcessMouseScroll(static_cast<float>(yoffset));
 }
