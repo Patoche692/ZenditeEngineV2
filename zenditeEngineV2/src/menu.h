629d227e327ea1b143c18d9cd79a64ffc743601a
Izendi <cjwinters1404@gmail.com>
2024-02-14 15:17:38 +0100

    basic menu example added

--- zenditeEngineV2/src/menu.h
+++ zenditeEngineV2/src/menu.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <imgui/imgui.h>
 #include <imgui/imgui_impl_glfw_gl3.h>
+#include "ECS/Components.h"
 
 void imGuiSetup(GLFWwindow* window)
 {
@@ -9,43 +10,31 @@ void imGuiSetup(GLFWwindow* window)
 	ImGui::StyleColorsDark();
 }
 
-void genMenu_1(bool& toggle, bool& wireframe, bool& rotation)
+void genMenu_1(c_Transform& posData)
 {
 	//Create IMGUI menu:
 	ImGui_ImplGlfwGL3_NewFrame();
 	//ImGui::NewFrame();
 
 	ImGui::Begin("GUI");
+	// --------------------------------------------------------------
+
+
 	ImGui::Separator();
-	if (ImGui::Button("Toggle Wireframe"))
+
+	if (ImGui::SliderFloat3("My Vec3 Slider", &posData.pos[0], -5.0f, 5.0f));
 	{
-		if (wireframe == false)
-		{
-			wireframe = true;
-		}
-		else
-		{
-			wireframe = false;
-		}
+		// The slider was used; myVec3 has been updated.
+		// You can handle the change here if needed.
 	}
-	ImGui::NewLine();
-	if (ImGui::Button("Toggle Rotation"))
-	{
-		if (rotation)
-		{
-			//glDisable(GL_DEPTH_TEST);
-			rotation = false;
-		}
-		else
-		{
-			//glEnable(GL_DEPTH_TEST);
-			rotation = true;
-		}
-	}
+
 	ImGui::NewLine();
 
+
+	// ----------------------------------------------------------
 	ImGui::End();
 	ImGui::Render();
 	ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
 
 }
+
